6b208347dd7234bba10eb47146701c94684e7cf7
Jose Cassios <cassios.torres.010@gmail.com>
2024-04-21 10:32:51 -0300

    feat: Implementing sound effects

--- main.py
+++ main.py
@@ -8,13 +8,22 @@ pygame.font.init()
 pygame.mixer.init() 
 
 mainDirectory = os.path.dirname(__file__)
+
 musicDirectory = os.path.join(mainDirectory, 'sounds/music')
+sfxDirectory = os.path.join(mainDirectory, 'sounds/sfx')
 fontsDirectory = os.path.join(mainDirectory, 'fonts')
 
 backgroundMusic = pygame.mixer.music
 backgroundMusic.load(os.path.join(musicDirectory, 'user.mp3'))
 backgroundMusic.set_volume(0.5)
-backgroundMusic.play()
+backgroundMusic.play(-1)
+
+hitSFX = pygame.mixer.Sound(os.path.join(sfxDirectory, 'hit.wav'))
+scoreSFX = pygame.mixer.Sound(os.path.join(sfxDirectory, 'score.wav'))
+wallSFX = pygame.mixer.Sound(os.path.join(sfxDirectory, 'wall.wav'))
+hitSFX.set_volume(0.2)
+scoreSFX.set_volume(0.2)
+wallSFX.set_volume(0.2)
 
 screenWidth = 640
 screenHeight = 480
@@ -84,20 +93,24 @@ while True:
     player2 = pygame.draw.rect(screen, (255,0,0), (screenWidth - 60, player2PosY, 20, 70))
         
     if ball.colliderect(leftBoundary):
+        scoreSFX.play()
         scorePlayer2 += 1
         ballPosX = screenWidth/2
         ballPosY = screenHeight/2
         ballSpeedX = abs(ballSpeedX)
     if ball.colliderect(rightBoundary):
-        scorePlayer1+=1
+        scoreSFX.play()
+        scorePlayer1 += 1
         ballPosX = screenWidth/2
         ballPosY = screenHeight/2
         ballSpeedX = -abs(ballSpeedX)
         
     if ball.colliderect(upperBoundary) or ball.colliderect(bottomBoundary):
+        wallSFX.play()
         ballSpeedY = -ballSpeedY
 
     if ball.colliderect(player1) or ball.colliderect(player2):
+        hitSFX.play()
         ballSpeedX = -ballSpeedX
         
     ballPosX += ballSpeedX
